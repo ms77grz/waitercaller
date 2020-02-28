6df1f97cfe055021fefbe4b64fd769f27d2e50fa
ms77grz <ms77grz@gmail.com>
2020-02-28 12:09:53 +0300

    Implementing secure password storage

--- waitercaller.py
+++ waitercaller.py
@@ -1,7 +1,8 @@
 import config
 from flask import Flask, render_template, redirect, url_for, request
-from flask_login import LoginManager, login_required, login_user
+from flask_login import LoginManager, login_required, login_user, logout_user
 from mockdbhelper import MockDBHelper as DBHelper
+from passwordhelper import PasswordHelper
 from user import User
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.secret_key = config.SECRET_KEY
 login_manager = LoginManager(app)
 
 DB = DBHelper()
+PH = PasswordHelper()
 
 
 @login_manager.user_loader
@@ -22,12 +24,33 @@ def load_user(user_id):
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_password = DB.get_user(email)
+    if stored_password and PH.validate_password(password, stored_password['salt'], stored_password['hashed']):
         user = User(email)
-        login_user(user)
-        return redirect(url_for('account'))
-    return home()
+        login_user(user,remember=True)
+        return redirect(url_for("account"))
+    return redirect(url_for("home"))
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for("home"))
+    if DB.get_user(email):
+        return redirect(url_for("home"))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for("home"))
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/")
